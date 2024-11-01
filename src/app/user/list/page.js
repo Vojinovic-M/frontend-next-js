39ceb19d794cd933a5ee07b68b65ba7d8cda8c78
Marko Vojinovic <146836999+Vojinovic-M@users.noreply.github.com>
2024-11-01 23:20:03 +0100

    Initial commit

--- src/app/user/list/page.js
+++ src/app/user/list/page.js
@@ -1,13 +1,49 @@
+'use client';
 import Link from "next/link";
+import useListData from "@/hooks/useListData";
+import {useEffect, useState} from "react";
+import {Spinner} from "reactstrap";
+import DataTable from "react-data-table-component";
+
+export const tableColumns = [
+    {
+        name: 'First Name',
+        selector: (row) => `${row.firstName}`,
+        sortable: false
+    },
+    {
+        name: 'Last Name',
+        selector: (row) => `${row.lastName}`,
+        sortable: false
+    }
+]
 
 export default function UserList() {
 
+    const {pageNumber, setPageNumber} = useState(1);
+    const {pageSize, setPageSize} = useState(10);
 
+    const {getData, loading, data} = useListData(`user/get-page-list?pageNumber=${pageNumber-1}&pageSize=${pageSize}`);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    console.log(data);
     return (
         <>
-            <h2>User List Page!</h2>
-            <br/>
-            <Link href="/user/create">Go to User Create page</Link>
+            <DataTable data={data.users}
+                       columns={tableColumns}
+                       striped={true}
+                       noHeader={true}
+                       pagination
+                       paginationServer
+                       progressPending={loading}
+                       paginationTotalRows={data.totalElements}
+                       progressComponent={<Spinner color="danger">Loading...</Spinner>}
+                       paginationPerPage={10}
+                       highlightOnHover
+            </>
         </>
     );
 }
